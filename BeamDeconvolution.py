cb06c14a6c29f45ae2571e5a76be35c33507296f
Joshua Albert <albert@strw.leidenuniv.nl>
2016-06-29 15:42:52 +0200

    added docs

--- BeamDeconvolution.py
+++ BeamDeconvolution.py
@@ -2,76 +2,89 @@ import numpy as np
 from scipy.optimize import minimize
 
 def ecliptic2quadratic(xc,yc,bmaj,bmin,pa,k=np.log(2)):
-	'''a*x**2 + b*x*y + c*y**2 + d*x + e*y + f = k
-	pa in deg'''
+    '''a*x**2 + b*x*y + c*y**2 + d*x + e*y + f = k
+    pa in deg'''
 
-	#unrotated solution
-	a0 = k/(bmaj/2.)**2
-	c0 = k/(bmin/2.)**2
-	theta = (pa + 90.)*np.pi/180.
-	#Rotated Solution
-	cos2 = np.cos(theta)**2
-	sin2 = np.sin(theta)**2
-	A = (a0*cos2 + c0*sin2)
-	C = (c0*cos2 + a0*sin2)
-	B = (a0 - c0 )*np.sin(2.*theta)
-	#Now move center
-	D = -2.*A*xc - B*yc
-	E = -2.*C*yc - B*xc
-	F = A*xc**2 + B*xc*yc + C*yc**2
-	return A,B,C,D,E,F
+    #unrotated solution
+    a0 = k/(bmaj/2.)**2
+    c0 = k/(bmin/2.)**2
+    theta = (pa + 90.)*np.pi/180.
+    #Rotated Solution
+    cos2 = np.cos(theta)**2
+    sin2 = np.sin(theta)**2
+    A = (a0*cos2 + c0*sin2)
+    C = (c0*cos2 + a0*sin2)
+    B = (a0 - c0 )*np.sin(2.*theta)
+    #Now move center
+    D = -2.*A*xc - B*yc
+    E = -2.*C*yc - B*xc
+    F = A*xc**2 + B*xc*yc + C*yc**2
+    return A,B,C,D,E,F
 
 def quad2ecliptic(A,B,C,k=np.log(2)):
-	A /= k
-	B /= k
-	C /= k
-	D1 = np.sqrt(A**2-2*A*C+B**2+C**2)
-	A0 = (-D1 + A + C)/2.
-	C0 = (D1 + A + C)/2.
-	D2 = D1 + A - C
-	D3 = 2*D2*D1
-	if D3 < 0 or A0 < 0 or C0 < 0:
-	#	print "No real ecliptic coordinates from quadratic"
-		return None
-	if (D2 == 0) or (D3 - B*np.sqrt(D3) == 0):
-		#print "circle"
-		theta = np.pi/2.
-	else:
-		theta = 2.*np.arctan(B/D2 - np.sqrt(D3)/D2)
-	bmaj = 2./np.sqrt(A0)
-	bmin = 2./np.sqrt(C0)
-	bpa = (theta - np.pi/2.)*180/np.pi#degs
-	bpa = np.mod(bpa,180.)-90.
-#	while bpa < 0:#to (0,180)
-#		bpa += 180.
-#	while bpa > 180.:
-#		bpa -= 180.
-        def chi2(b,ak,bk,ck):
-            a,b,c,d,e,f = ecliptic2quadratic(0.,0.,b[0],b[1],b[2])
-            return (a-ak)**2 + (b-bk)**2 + (c-ck)**2
-        res = minimize(chi2,(bmaj,bmin,bpa),args=(A,B,C),method='Powell')
-	return res.x
-	
+    '''Takes quadratic parameters (A,B,C) and 
+    does a linear solve and then a chi-squared solve for non-linear fit
+    returns 
+        bmaj, bmin -> (units dependent on how A,B,C derived)
+        bpa -> (deg)
+    if not possible returns None'''
+    A /= k
+    B /= k
+    C /= k
+    D1 = np.sqrt(A**2-2*A*C+B**2+C**2)
+    A0 = (-D1 + A + C)/2.
+    C0 = (D1 + A + C)/2.
+    D2 = D1 + A - C
+    D3 = 2*D2*D1
+    if D3 < 0 or A0 < 0 or C0 < 0:
+    #    print "No real ecliptic coordinates from quadratic"
+        return None
+    if (D2 == 0) or (D3 - B*np.sqrt(D3) == 0):
+        #print "circle"
+        theta = np.pi/2.
+    else:
+        theta = 2.*np.arctan(B/D2 - np.sqrt(D3)/D2)
+    bmaj = 2./np.sqrt(A0)
+    bmin = 2./np.sqrt(C0)
+    bpa = (theta - np.pi/2.)*180/np.pi#degs
+    bpa = np.mod(bpa,180.)-90.
+#    while bpa < 0:#to (0,180)
+#        bpa += 180.
+#    while bpa > 180.:
+#        bpa -= 180.
+    def chi2(b,ak,bk,ck):
+        a,b,c,d,e,f = ecliptic2quadratic(0.,0.,b[0],b[1],b[2])
+        return (a-ak)**2 + (b-bk)**2 + (c-ck)**2
+    res = minimize(chi2,(bmaj,bmin,bpa),args=(A,B,C),method='Powell')
+    return res.x
+    
 
 def deconvolve(A1,B1,C1,A2,B2,C2):
-	'''Solves analytically G(A1,B1,C1) = convolution(G(A2,B2,C2), G(Ak,Bk,Ck))
-	for Ak,Bl,Ck
-	Returns None if delta function'''
-	D = B1**2 - 2*B1*B2 + B2**2 - 4*A1*C1 + 4* A2* C1 + 4* A1* C2 - 4* A2* C2
-	if (np.abs(D) < 10*(1-2./3.-1./3.)):
+    '''Solves analytically G(A1,B1,C1) = convolution(G(A2,B2,C2), G(Ak,Bk,Ck))
+    Returns Ak,Bk,Ck
+    A,B,C are quadratic parametrization.
+    If you have bmaj,bmin,bpa, then get A,B,C = ecliptic2quadratic(0,0,bmaj,bmin,bpa)
+    
+    Returns (None,None,None) if solution is delta function'''
+    D = B1**2 - 2*B1*B2 + B2**2 - 4*A1*C1 + 4* A2* C1 + 4* A1* C2 - 4* A2* C2
+    if (np.abs(D) < 10*(1-2./3.-1./3.)):
 
-		#print "Indefinite... invertibles"
-		return None#delta function
-	if (D<0.):
-		#print "Inverse Gaussian, discriminant D:",D
-		pass
-	Ak = (-A2* B1**2 + A1* B2**2 + 4* A1* A2* C1 - 4* A1* A2* C2)/D
-	Bk = (-B1**2 *B2 + B1* B2**2 + 4* A1* B2* C1 - 4* A2* B1* C2)/D
-	Ck = (B2**2 *C1 - B1**2 *C2 + 4* A1* C1* C2 - 4* A2* C1* C2)/D
+        #print "Indefinite... invertibles"
+        return (None,None,None)#delta function
+    if (D<0.):
+        #print "Inverse Gaussian, discriminant D:",D
+        pass
+    Ak = (-A2* B1**2 + A1* B2**2 + 4* A1* A2* C1 - 4* A1* A2* C2)/D
+    Bk = (-B1**2 *B2 + B1* B2**2 + 4* A1* B2* C1 - 4* A2* B1* C2)/D
+    Ck = (B2**2 *C1 - B1**2 *C2 + 4* A1* C1* C2 - 4* A2* C1* C2)/D
 
-	return Ak,Bk,Ck
+    return Ak,Bk,Ck
 
 def findCommonBeam(beams):
+    '''Given a list of beams where each element of beams is a dictionary having standard casa format:
+    {'major':{'value':<>,'unit':<>},'minor':{'value':<>,'unit':<>},'bpa':{'value':<>,'unit':<>}}
+
+    return the beam parameters '''
     beams_array = []
     for b in beams:
         beams_array.append([b['major']['value'],b['minor']['value'],b['pa']['value']])
@@ -101,7 +114,7 @@ def findCommonBeam(beams):
                 break
         i += 1
     if cb is None:
-        Area_init = Areas[idxMaxArea]
+        Area_init = Areas[idxMaxArea]*1.05
         inc = 1.05#15 iters in area
         works = False
         Area = Area_init
@@ -121,30 +134,37 @@ def findCommonBeam(beams):
                         A2,B2,C2,D,E,F = ecliptic2quadratic(0.,0.,beams_array[i,0],beams_array[i,1],beams_array[i,2])
                         Ak,Bk,Ck = deconvolve(A1,B1,C1,A2,B2,C2)
                         print Ak,Bk,Ck
+                        if Ak is None:
+                            print "Failed convolve"
+                            cb = None
+                            break
+
                         try:
                             b = quad2ecliptic(Ak,Bk,Ck,k=np.log(2))
                             if b is None:
-                                pass
+                                cb = None
+                                break
+                                
                             else:
-                                "convolve possible:",b
+                                print "Transform possible:",b
                         except:
-                            "Failed convolve"
+                            "Transform impossible:"
                             cb = None
                             break
                         i += 1
                     if cb is not None:
                         work = True
-
             Area *= inc
+    return cb
 
             
         
 
-	
+    
 def fftGaussian(A,B,C,X,Y):
-	D = 4*A*C-B**2
-	return 2*np.pi/np.sqrt(D)*np.exp(-4*np.pi/D*(-C*X**2 +B*X*Y -A*Y**2))
+    D = 4*A*C-B**2
+    return 2*np.pi/np.sqrt(D)*np.exp(-4*np.pi/D*(-C*X**2 +B*X*Y -A*Y**2))
 
 def gaussian(A,B,C,X,Y):
-	return np.exp(-A*X**2 - B*X*Y - C*Y**2)
+    return np.exp(-A*X**2 - B*X*Y - C*Y**2)
 
