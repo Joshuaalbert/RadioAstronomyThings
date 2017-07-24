3596fd463fa6f148cc7100caaf1b276334b51790
joshuaalbert <albert@strw.leidenuniv.nl>
2017-07-24 15:11:18 +0200

    test for new MIP solver

--- src/rathings/tests/test_rathings.py
+++ src/rathings/tests/test_rathings.py
@@ -1,5 +1,5 @@
 from rathings.phase_unwrap import phase_unwrapp1d
-from rathings.tec_solver import l1data_l2model_solve
+from rathings.tec_solver import l1data_l2model_solve, l1_lpsolver
 import numpy as np
 
 def test_phase_unwrapp1d():
@@ -21,11 +21,24 @@ def test_tec_solver():
     times = np.arange(1)
     freqs = np.linspace(110e6,170e6,100)
     cs = times*0
-    tec = times*0 + 0.01
-    phase_ = np.multiply.outer(np.ones(len(freqs)),cs) + 8.44797256e-7*1e12*np.multiply.outer(1./freqs,tec)
-    phase = phase_ + 10*np.pi/180.*np.random.normal(size=[len(freqs),len(times)])
+    tec = times*0 + 0.001
+    phase_ = np.multiply.outer(np.ones(len(freqs)),cs) + 8.44797256e-7*1e16*np.multiply.outer(1./freqs,tec)
+    phase = phase_ + 2*np.pi/180.*np.random.normal(size=[len(freqs),len(times)])
+    phase[50:70,:] +=  6*np.pi/180.*np.random.normal(size=[20,len(times)])
+    
+    #wrap the phsae
     phase = np.angle(np.exp(1j*phase))
 
-    m = l1data_l2model_solve(phase,freqs,10)
-    rec_phase = np.multiply.outer(np.ones(len(freqs)),m[:,1]) + 8.44797256e-7*1e12*np.multiply.outer(1./freqs,m[:,0])
-    assert np.all(np.abs(rec_phase - phase_)<0.01)
+    m = l1data_l2model_solve(phase,freqs,5)
+    rec_phase = np.multiply.outer(np.ones(len(freqs)),m[:,1]) + 8.44797256e-7*1e16*np.multiply.outer(1./freqs,m[:,0])
+    #assert np.all(np.abs(rec_phase - phase_)<0.01)
+    sigma_out, tec,cs = l1_lpsolver(phase[:,0],freqs,fout=0.5)
+    print("outlier thresh: {} deg, tec: {}, cs: {}".format(sigma_out*180./np.pi, tec, cs))
+
+    rec_phase = 8.44797256e-7*1e16*np.multiply.outer(1./freqs,tec) + cs
+#    import pylab as plt
+#    plt.plot(freqs,phase)
+#    plt.plot(freqs,rec_phase)
+#    plt.show()
+    print("MAE: {}".format(np.sum(np.abs(rec_phase - phase))/len(freqs)))
+
